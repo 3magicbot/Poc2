4aa9b7e608f50571ce5ea0bf30899a79d0531140
azure.learnnow <azure.learnnow@gmail.com>
2023-06-23 09:40:07 +0530

    Add project files.

---
+++ Dialogs/MainDialog.cs
@@ -0,0 +1,121 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.18.1
+
+using MagicBot001.CognitiveModels;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static MagicBot001.CognitiveModels.FlightBooking;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Input;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Templates;
+
+namespace MagicBot001.Dialogs
+{
+    public class MainDialog : ComponentDialog
+    {
+        //private readonly RegexRecognizer _luisRecognizer;
+        private readonly ILogger _logger;
+
+        // Dependency injection uses this constructor to instantiate MainDialog
+        public MainDialog( BookingDialog bookingDialog, ILogger<MainDialog> logger)
+            : base(nameof(MainDialog))
+        {
+           // _luisRecognizer = luisRecognizer;
+            _logger = logger;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(bookingDialog);
+
+            var waterfallSteps = new WaterfallStep[]
+            {
+                IntroStepAsync,
+                ActStepAsync,
+                FinalStepAsync,
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        // Shows a warning if the requested From or To cities are recognized as entities but they are not in the Airport entity list.
+        // In some cases LUIS will recognize the From and To composite entities as a valid cities but the From and To Airport values
+        // will be empty if those entity values can't be mapped to a canonical item in the Airport.
+        private static async Task ShowWarningForUnsupportedCities(ITurnContext context, FlightBooking luisResult, CancellationToken cancellationToken)
+        {
+            var unsupportedCities = new List<string>();
+
+            var fromEntities = luisResult.FromEntities;
+            if (!string.IsNullOrEmpty(fromEntities.From) && string.IsNullOrEmpty(fromEntities.Airport))
+            {
+                unsupportedCities.Add(fromEntities.From);
+            }
+
+            var toEntities = luisResult.ToEntities;
+            if (!string.IsNullOrEmpty(toEntities.To) && string.IsNullOrEmpty(toEntities.Airport))
+            {
+                unsupportedCities.Add(toEntities.To);
+            }
+
+            if (unsupportedCities.Any())
+            {
+                var messageText = $"Sorry but the following airports are not supported: {string.Join(',', unsupportedCities)}";
+                var message = MessageFactory.Text(messageText, messageText, InputHints.IgnoringInput);
+                await context.SendActivityAsync(message, cancellationToken);
+            }
+        }
+
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+ 
+            // Use the text provided in FinalStepAsync or the default if it is the first time.
+            var messageText = stepContext.Options?.ToString() ?? "What can I help you with today?\nSay something like \"Book a flight from Paris to Berlin on March 22, 2020\"";
+            var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            string mytext = stepContext.Result.ToString();
+            
+
+            return await stepContext.NextAsync( null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // If the child dialog ("BookingDialog") was cancelled, the user failed to confirm or if the intent wasn't BookFlight
+            // the Result here will be null.
+            if (stepContext.Result is BookingDetails result)
+            {
+                // Now we have all the booking details call the booking service.
+
+                // If the call to the booking service was successful tell the user.
+
+                var timeProperty = new TimexProperty(result.TravelDate);
+                var travelDateMsg = timeProperty.ToNaturalLanguage(DateTime.Now);
+                var messageText = $"I have you booked to {result.Destination} from {result.Origin} on {travelDateMsg}";
+                var message = MessageFactory.Text(messageText, messageText, InputHints.IgnoringInput);
+                await stepContext.Context.SendActivityAsync(message, cancellationToken);
+            }
+
+            // Restart the main dialog with a different message the second time around
+            var promptMessage = "What else can I do for you?";
+            return await stepContext.ReplaceDialogAsync(InitialDialogId, promptMessage, cancellationToken);
+        }
+    }
+}
